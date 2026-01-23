1a257ba7701ed6f16e80454feee36207c34a92de
Jace Rodgers <jaransas2008@gmail.com>
2026-01-22 19:45:28 -0600

    linting

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -17,18 +17,18 @@ public class FieldConstants {
     /** Measured from the inside of starting line */
     public static final Distance startingLineX = Inches.of(299.438);
 
-    final static public Rectangle blueAlliance =
+    static final public Rectangle blueAlliance =
         new Rectangle("Blue Alliacne", new Pose2d(2, 4.05, Rotation2d.kZero), 4.0, 8.1);
-    final static public Rectangle blueAllianceClimber = new Rectangle("Blue Alliacne Climber",
+    static final public Rectangle blueAllianceClimber = new Rectangle("Blue Alliacne Climber",
         new Pose2d(0.6281201839447021, 3.75, Rotation2d.kZero), 1.2, 1.1);
-    final static public Rectangle blueDropper =
+    static final public Rectangle blueDropper =
         new Rectangle("Blue Dropper", new Pose2d(5.7, 4.05, Rotation2d.kZero), 1.0, 1.7);
-    final static public Rectangle neutralZone =
+    static final public Rectangle neutralZone =
         new Rectangle("Neutral Zone", new Pose2d(8.25, 4.05, Rotation2d.kZero), 6.1, 8.1);
-    final static public Rectangle redDropper =
+    static final public Rectangle redDropper =
         new Rectangle("Red Dropper", new Pose2d(10.8, 4.05, Rotation2d.kZero), 1.0, 1.7);
-    final public static Rectangle redAlliance =
+    public final static Rectangle redAlliance =
         new Rectangle("Red Alliacne", new Pose2d(14.5, 4.05, Rotation2d.kZero), 4, 8.1);
-    final static public Rectangle redAllianceClimber =
+    static final public Rectangle redAllianceClimber =
         new Rectangle("Red Alliacne Climber", new Pose2d(15.87, 3.75, Rotation2d.kZero), 1.2, 1.1);
 }
