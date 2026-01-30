8849cf49cde93bc6a612a6f99dff63d547a0d70d
Everett Walker <everettwalkerj@gmail.com>
2026-01-29 18:40:35 -0600

    Shooter (#23)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,6 +2,7 @@ package frc.robot;
 
 import static edu.wpi.first.units.Units.Inches;
 import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.ctre.phoenix6.signals.SensorDirectionValue;
 import com.studica.frc.AHRS.NavXComType;
@@ -240,6 +241,27 @@ public final class Constants {
                 .finish(),
         };
         // @formatter:on
+    }
+
+    /** Shooter Constants */
+    public static final class Shooter {
+        /** ID for Shooter Motor 1 */
+        public static final int motor1ID = 36;
+        /** ID for Shooter Motor 2 */
+        public static final int motor2ID = 37;
+
+        /** Motor Invert for Shooter Motors */
+        public static final InvertedValue shooterMotorInvert = InvertedValue.Clockwise_Positive;
+        /** Motor Alignment for Shooter Motors */
+        public static final MotorAlignmentValue shooterMotorAlignment = MotorAlignmentValue.Opposed;
+        /** Neutral Mode for Shooter Motors */
+        public static final NeutralModeValue shooterNeutralMode = NeutralModeValue.Brake;
+
+        public static final double shooterKS = 0.1;
+        public static final double shooterKV = 0.12;
+        public static final double shooterKP = 0.11;
+        public static final double shooterKI = 0.0;
+        public static final double shooterKD = 0.0;
     }
 
     public static boolean disableHAL = false;
---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSim.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems.shooter;
+
+/**
+ * Shooter Sim Implementation
+ */
+public class ShooterSim implements ShooterIO {
+    @Override
+    public void updateInputs(ShooterInputs inputs) {
+
+    }
+
+    @Override
+    public void runShooterVelocity(double velocity) {
+
+    }
+
+}
