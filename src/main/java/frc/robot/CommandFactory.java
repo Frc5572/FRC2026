884fbf4c8a3fd38d93a6eebf769553f782a90f62
Gavin Murray <m67115586@gmail.com>
2026-02-17 17:24:53 -0600

    Fixed Syntax Errors on Supplier

--- src/main/java/frc/robot/CommandFactory.java
+++ src/main/java/frc/robot/CommandFactory.java
@@ -19,20 +19,21 @@ public class CommandFactory {
      * Sets the turret's target to the left or right based off of its closest distence, then sets
      * the angle of the hood, then the velocity of the shooter, then it shoots.
      */
-    public static Command autoPass(Supplier<Pose2d> swervePose, Turret turret, AdjustableHood hood,
-        Shooter shooter) {
+    public static Command autoPass(Supplier<Pose2d> supplierSwervePose, Turret turret,
+        AdjustableHood hood, Shooter shooter) {
         return Commands.run(() -> {
+            Pose2d swervePose = supplierSwervePose.get();
             Distance leftDistance =
-                Meters.of(Hub.nearLeftCorner.getDistance(((Pose2d) swervePose).getTranslation()));
+                Meters.of(Hub.nearLeftCorner.getDistance((swervePose).getTranslation()));
             Distance rightDistance =
-                Meters.of(Hub.nearRightCorner.getDistance(((Pose2d) swervePose).getTranslation()));
+                Meters.of(Hub.nearRightCorner.getDistance((swervePose).getTranslation()));
             if (leftDistance.in(Meters) < rightDistance.in(Meters)) {
                 Angle leftDistanceGoal = Rotations.of(Hub.nearLeftCorner
-                    .minus(((Pose2d) swervePose).getTranslation()).getAngle().getRotations());
+                    .minus(swervePose.getTranslation()).getAngle().getRotations());
                 turret.setGoal(leftDistanceGoal);
             } else {
                 Angle rightDistanceGoal = Rotations.of(Hub.nearRightCorner
-                    .minus(((Pose2d) swervePose).getTranslation()).getAngle().getRotations());
+                    .minus(swervePose.getTranslation()).getAngle().getRotations());
                 turret.setGoal(rightDistanceGoal);
             }
             hood.setGoal(Rotations.of(Constants.AdjustableHood.passingAngle));
