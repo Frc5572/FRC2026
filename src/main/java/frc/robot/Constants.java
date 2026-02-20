5b0c9f48aaee0298b2c28be25dc3035fcddff263
Gavin Murray <156257966+GavTechMaster@users.noreply.github.com>
2026-02-20 10:13:13 -0600

    Feeding (#78)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,6 +9,7 @@ import static edu.wpi.first.units.Units.RotationsPerSecond;
 import java.util.List;
 import com.ctre.phoenix6.signals.GravityTypeValue;
 import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.ctre.phoenix6.signals.SensorDirectionValue;
 import com.studica.frc.AHRS.NavXComType;
@@ -373,6 +374,8 @@ public final class Constants {
         public static final SensorDirectionValue hoodCANCoderInvert =
             SensorDirectionValue.Clockwise_Positive;
         public static final double hoodCANcoderDiscontinuity = 0.5;
+
+        public static final double passingAngle = 0.0;
     }
 
     /**
@@ -558,6 +561,21 @@ public final class Constants {
         /** ID for Shooter Motor 2 */
         public static final int motor2ID = 12;
 
+        /** Motor Invert for Shooter Motors */
+        public static final InvertedValue shooterMotorInvert = InvertedValue.Clockwise_Positive;
+        /** Motor Alignment for Shooter Motors */
+        public static final MotorAlignmentValue shooterMotorAlignment = MotorAlignmentValue.Opposed;
+        /** Neutral Mode for Shooter Motors */
+        public static final NeutralModeValue shooterNeutralMode = NeutralModeValue.Brake;
+
+        public static final double shooterKS = 0.1;
+        public static final double shooterKV = 0.12;
+        public static final double shooterKP = 0.11;
+        public static final double shooterKI = 0.0;
+        public static final double shooterKD = 0.0;
+
+        public static final double passingSpeed = 0.0;
+        public static final double shooterVelocity = 0.0;
 
         // @formatter:off
         public static final FlywheelConstants constants =
