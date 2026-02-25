0197a92734ab2bf5e08c3624a2679b31647ce631
Nirmal Kozak <nirmalkozak@gmail.com>
2026-02-24 19:35:39 -0600

    Added AutoCommandFactory

---
+++ src/main/java/frc/robot/AutoCommandFactory.java
@@ -0,0 +1,34 @@
+package frc.robot;
+
+import choreo.auto.AutoFactory;
+import frc.robot.subsystems.adjustable_hood.AdjustableHood;
+import frc.robot.subsystems.climber.Climber;
+import frc.robot.subsystems.indexer.Indexer;
+import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.shooter.Shooter;
+import frc.robot.subsystems.swerve.Swerve;
+import frc.robot.subsystems.turret.Turret;
+
+public class AutoCommandFactory {
+
+    AutoFactory autoFactory;
+    Swerve swerve;
+    AdjustableHood adjustableHood;
+    Climber climber;
+    Indexer indexer;
+    Intake intake;
+    Shooter shooter;
+    Turret turret;
+
+    public AutoCommandFactory(AutoFactory autoFactory, Swerve swerve, AdjustableHood adjustableHood,
+        Climber climber, Intake intake, Indexer indexer, Shooter shooter, Turret turret) {
+        this.autoFactory = autoFactory;
+        this.swerve = swerve;
+        this.adjustableHood = adjustableHood;
+        this.climber = climber;
+        this.indexer = indexer;
+        this.intake = intake;
+        this.shooter = shooter;
+        this.turret = turret;
+    }
+}
