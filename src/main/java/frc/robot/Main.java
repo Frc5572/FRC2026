e57692ce113055f486fe2bfecf4d24b6513d794a
wilsonwatson <wilsonwatson@utexas.edu>
2026-03-02 18:55:16 -0600

    remove rbfs

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -12,15 +12,11 @@ import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.Base64;
-import java.util.function.DoubleFunction;
-import java.util.function.DoubleUnaryOperator;
 import java.util.function.ToDoubleFunction;
 import javax.imageio.ImageIO;
 import frc.robot.ShotData.ShotEntry;
 import frc.robot.math.interp2d.Interp2d;
 import frc.robot.math.interp2d.MulAdd;
-import frc.robot.math.interp2d.RbfInterp2d;
-import frc.robot.math.interp2d.Rbfs;
 import frc.robot.util.Tuples.Tuple3;
 import frc.robot.viz.DrawColorMap;
 
@@ -49,29 +45,6 @@ public final class Main {
         drawImage("interp2d", ShotData.entries, "distance (ft)", ShotEntry::distanceFeet,
             "Flywheel Speed (rps)", ShotEntry::flywheelSpeedRps, "Hood Angle (deg)",
             ShotEntry::hoodAngleDeg, ShotData.mulAdd);
-        double[] epss = new double[] {0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0};
-        @SuppressWarnings("unchecked")
-        DoubleFunction<DoubleUnaryOperator>[] rbfs =
-            new DoubleFunction[] {Rbfs::bump, Rbfs::gaussian, Rbfs::inverseMultiquadric,
-                Rbfs::inverseQuadratic, Rbfs::multiquadric, Rbfs::rth};
-        String[] names = new String[] {"bump", "gaussian", "inverseMultiquadric",
-            "inverseQuadratic", "multiquadric", "rth"};
-        for (double eps : epss) {
-            DoubleUnaryOperator[] rbfs_ =
-                Arrays.stream(rbfs).map((x) -> x.apply(eps)).toArray(DoubleUnaryOperator[]::new);
-            for (int i = 0; i < rbfs_.length; i++) {
-                drawImage(names[i] + "_" + eps, ShotData.entries, "distance (ft)",
-                    ShotData.ShotEntry::distanceFeet, "Flywheel Speed (rps)",
-                    ShotData.ShotEntry::flywheelSpeedRps, "Hood Angle (deg)",
-                    ShotData.ShotEntry::hoodAngleDeg, rbfs_[i]);
-            }
-        }
-        for (int i = 0; i < 6; i++) {
-            drawImage("polyharmonic_" + i, ShotData.entries, "distance (ft)",
-                ShotData.ShotEntry::distanceFeet, "Flywheel Speed (rps)",
-                ShotData.ShotEntry::flywheelSpeedRps, "Hood Angle (deg)",
-                ShotData.ShotEntry::hoodAngleDeg, Rbfs.polyharmonicSpline(i));
-        }
     }
 
     private static <T> void drawImage(String name, T[] data, String xName,
@@ -88,54 +61,6 @@ public final class Main {
         try {
             res = DrawColorMap.draw(x -> zFunc.applyAsDouble(interp.query(x).value()), xMin, xMax,
                 yMin, yMax);
-            key = DrawColorMap.key();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String b64Res = imageToBase64(res._0());
-        String b64Key = imageToBase64(key);
-        double zMin = res._1();
-        double zMax = res._2();
-
-        StringBuilder builder = new StringBuilder();
-
-        for (var item : data) {
-            double x = xFunc.applyAsDouble(item);
-            double y = yFunc.applyAsDouble(item);
-            double u = (x - xMin) / (xMax - xMin);
-            double v = 1.0 - (y - yMin) / (yMax - yMin);
-            builder.append(circleSvg.replaceAll("\\{cx\\}", (u * 200.0 + 60.0) + "")
-                .replaceAll("\\{cy\\}", (v * 200.0 + 10.0) + ""));
-        }
-
-        DecimalFormat df = new DecimalFormat("#.##");
-
-        try (var writer = new BufferedWriter(new FileWriter(name + ".svg"))) {
-            writer.write(svgTemplate.replaceAll("\\{b64Res\\}", b64Res)
-                .replaceAll("\\{b64Key\\}", b64Key).replaceAll("\\{xMin\\}", df.format(xMin))
-                .replaceAll("\\{xMax\\}", df.format(xMax)).replaceAll("\\{yMin\\}", df.format(yMin))
-                .replaceAll("\\{yMax\\}", df.format(yMax)).replaceAll("\\{zMin\\}", df.format(zMin))
-                .replaceAll("\\{zMax\\}", df.format(zMax)).replaceAll("\\{xName\\}", xName)
-                .replaceAll("\\{yName\\}", yName).replaceAll("\\{zName\\}", zName)
-                .replaceAll("\\{circles\\}", builder.toString()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static <T> void drawImage(String name, T[] data, String xName,
-        ToDoubleFunction<T> xFunc, String yName, ToDoubleFunction<T> yFunc, String zName,
-        ToDoubleFunction<T> zFunc, DoubleUnaryOperator rbf) {
-        double xMin = Arrays.stream(data).mapToDouble(xFunc).min().getAsDouble();
-        double xMax = Arrays.stream(data).mapToDouble(xFunc).max().getAsDouble();
-        double yMin = Arrays.stream(data).mapToDouble(yFunc).min().getAsDouble();
-        double yMax = Arrays.stream(data).mapToDouble(yFunc).max().getAsDouble();
-
-        RbfInterp2d interp = new RbfInterp2d(data, xFunc, yFunc, zFunc, rbf);
-        Tuple3<BufferedImage, Double, Double> res = null;
-        BufferedImage key = null;
-        try {
-            res = DrawColorMap.draw(x -> interp.query(x.getX(), x.getY()), xMin, xMax, yMin, yMax);
             key = DrawColorMap.key();
         } catch (IOException e) {
             e.printStackTrace();
