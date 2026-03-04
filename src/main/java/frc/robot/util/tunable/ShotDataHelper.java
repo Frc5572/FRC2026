3cd16ce6635e225e27a93ff70022bc7771085271
Nirmal Kozak <nirmalkozak@gmail.com>
2026-03-04 15:04:34 -0600

    added shotDataHelper

--- src/main/java/frc/robot/util/tunable/ShotDataHelper.java
+++ src/main/java/frc/robot/util/tunable/ShotDataHelper.java
@@ -1,5 +1,18 @@
 package frc.robot.util.tunable;
 
-public class ShotDataHelper {
+/** Utility tunable for testing out different shot parameters. */
+public class ShotDataHelper implements Tunable {
+
+    /** Hood angle in degrees */
+    public double hoodAngle;
+    /** Flywheel speed in rotations per second */
+    public double flywheelSpeed;
+    /** Distance from target in feet */
+    public double distanceFromTarget;
+
+    public ShotDataHelper() {
+        Tunable.setupTunable("/ShotDataHelper", this, ShotDataHelper.class, () -> {
+        });
+    }
 
 }
