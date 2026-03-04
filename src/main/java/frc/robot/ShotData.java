537293eabdfd4d1f29bdb174e559732a6ad4cc84
Nirmal Kozak <nirmalkozak@gmail.com>
2026-03-04 15:17:00 -0600

    added more imports

--- src/main/java/frc/robot/ShotData.java
+++ src/main/java/frc/robot/ShotData.java
@@ -4,6 +4,7 @@ import static edu.wpi.first.units.Units.Meters;
 import frc.robot.math.interp2d.BilinearSurrogate;
 import frc.robot.math.interp2d.Interp2d;
 import frc.robot.math.interp2d.MulAdd;
+import frc.robot.math.interp2d.Range;
 import frc.robot.math.interp2d.RangeOf;
 
 /** Data for flywheel, distance, and hood angle that results in a successful shot. */
