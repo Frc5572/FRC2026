1f38f262d79110a6bace071f834392806bd5f204
Nirmal Kozak <nirmalkozak@gmail.com>
2026-04-25 15:57:35 -0500

    created a vertical lock for the robot when the left bumper is held down.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -282,6 +282,7 @@ public final class RobotContainer {
         // Constants.DriverControls.driverTranslationalMaxSpeed,
         // Constants.DriverControls.driverRotationalShootSpeed));
         driver.rightBumper().whileTrue(swerve.toggleSideLock());
+        driver.leftBumper().whileTrue(swerve.toggleVerticalLock());
     }
 
     private void setupOperator() {
