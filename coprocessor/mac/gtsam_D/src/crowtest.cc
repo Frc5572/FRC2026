dcbeea31caee21044e4317fbb62b20e21e18f566
Jace Rodgers <jaransas2008@gmail.com>
2026-05-12 12:12:20 -0500

    super cool json uploader

--- coprocessor/mac/gtsam_D/src/crowtest.cc
+++ coprocessor/mac/gtsam_D/src/crowtest.cc
@@ -1,13 +1,56 @@
-#define CPPHTTPLIB_THREAD_POOL_COUNT 1
-
 #include "httplib.hh"
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+std::string read_file(const std::string &path)
+{
+    std::ifstream file(path);
+
+    if (!file.is_open())
+    {
+        return "Failed to open file.";
+    }
+
+    std::stringstream buffer;
+
+    buffer << file.rdbuf();
+
+    return buffer.str();
+}
 
 int main()
 {
-    httplib::Server svr;
+    httplib::Server server;
 
-    svr.Get("/", [](const httplib::Request &, httplib::Response &res)
-            { res.set_content("hello", "text/plain"); });
+    // Serve HTML file
+    server.Get("/", [](const httplib::Request &,
+                       httplib::Response &res)
+               {
+        std::string html = read_file("/Users/jacerodgers/Code/FRC2026/coprocessor/mac/gtsam_D/src/index.html");
 
-    svr.listen("0.0.0.0", 8080);
+        res.set_content(html, "text/html"); });
+
+    // Upload endpoint
+    server.Post("/upload", [](const httplib::Request &req,
+                              httplib::Response &res)
+                {
+        std::ofstream out("uploaded.json");
+
+        out << req.body;
+
+        out.close();
+
+        std::cout << req.body << std::endl;
+
+        res.set_content(
+            "JSON uploaded successfully.",
+            "text/plain"
+        ); });
+
+    std::cout << "Server running at http://localhost:8080\n";
+
+    server.listen("0.0.0.0", 8080);
 }
