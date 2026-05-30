8db574d6ca95acfe05a38742b32b7147905b4db5
Jace Rodgers <jaransas2008@gmail.com>
2026-05-30 01:43:11 -0500

    maybe work

---
+++ src/main/java/frc/robot/localization/VisionObservations.java
@@ -0,0 +1,15 @@
+package frc.robot.localization;
+
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.Vector;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.numbers.N3;
+
+public record VisionObservations(Pose3d cameraPose, Transform3d robotToCamera,
+    double translationStdDev, double rotationStdDev, double timestamp) {
+
+    public Vector<N3> getStdDev() {
+        return VecBuilder.fill(translationStdDev, translationStdDev, rotationStdDev);
+    }
+}
