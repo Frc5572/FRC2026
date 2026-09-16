2745dcc1dcdbe9c90e9c1cace4a9d710f17009f8
GavTechMaster <m67115586@gmail.com>
2026-09-16 13:47:33 -0500

    Update Test

--- src/test/java/frc/robot/math/geometry/CircleTest.java
+++ src/test/java/frc/robot/math/geometry/CircleTest.java
@@ -1,13 +1,16 @@
 package frc.robot.math.geometry;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import edu.wpi.first.math.geometry.Translation2d;
 
 public class CircleTest {
     @Test
-    public void circleChange() {
+    public void circleBasics() {
         Circle testCircle = new Circle("Test", new Translation2d(0, 0), 10);
         assertTrue(testCircle.contains(new Translation2d(7, 7)));
+        testCircle.setRadius(7);
+        assertFalse(!testCircle.contains(new Translation2d(7, 7)));
     }
 }
